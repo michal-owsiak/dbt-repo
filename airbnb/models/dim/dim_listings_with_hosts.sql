4a094cecfd5e5f23894ac475e8f209d0f2345681
michal-owsiak <micowsiak@gmail.com>
2026-01-29 20:19:29 +0100

    dim_listings_with_hosts model created

---
+++ airbnb/models/dim/dim_listings_with_hosts.sql
@@ -0,0 +1,22 @@
+with l as (
+    select * from {{ ref('dim_listings_cleansed') }}
+), 
+
+h as (
+    select * from {{ ref('dim_hosts_cleansed') }}
+)
+
+select
+    l.listing_id,
+    l.listing_name,
+    l.room_type,
+    l.minimum_nights,
+    l.price,
+    h.host_id,
+    h.host_name,
+    h.is_superhost as host_is_superhost,
+    l.created_at,
+    greatest(l.updated_at, h.updated_at) as last_updated_at
+from l 
+left join h
+    on l.host_id = h.host_id;
